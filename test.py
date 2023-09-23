e40fd7cfc87a4b32c0ae9494f733ecdd5819ad69
ArgonQQQ <argonqqq1@mail.ru>
2023-09-23 18:55:09 +0400

    M1L4 - my translator

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-class Person:
-    age = 42
-    hair = "black"
-    eye = "green"
+import string
+from googletrans import Translator
+class Translator_text:
 
-person_Valera = Person()
 
-print(Person.hair)
+    def __init__(self, word_obj):
+        self.word = word_obj
+        self.russian_dict = {"я": "I", "яблоко": "apple", 'банан': "banana", 'город': 'city', 'человек': 'human'}
+        self.english_dict = {'program': 'программа', 'technology': 'технология', 'innovation': 'инновация',
+                        'super man': 'супермен'}
+    def translator_text(self):
+        if self.word[0].upper() in string.ascii_uppercase:
+            translate_word = self.translator.translate(self.word, dest='ru').text
+            return translate_word
+        elif self.word[0].upper() not in string.ascii_uppercase:
+            translate_word = self.translator.translate(self.word, dest='en').text
+            return translate_word
+
+
+translator1 = Translator("technology")
+print(translator1.translator_text())
